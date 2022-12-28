067b3307be9ff7d2305a209590093e1dd625a557
PranjalbaZala <65394646+PranjalbaZala@users.noreply.github.com>
2022-12-29 00:53:22 +0530

    Add files via upload

---
+++ IService1.cs
@@ -0,0 +1,145 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.ServiceModel.Web;
+using System.Text;
+
+namespace Project_User_Registration
+{
+    // NOTE: You can use the "Rename" command on the "Refactor" menu to change the interface name "IService1" in both code and config file together.
+    [ServiceContract]
+    public interface IService1
+    {
+        //For insert operation for user starts here
+        [OperationContract]
+        string Insert(InsertUser user);
+
+        //for search
+        [OperationContract]
+        gettestdata GetInfo();
+
+        [OperationContract]
+        string Update(UpdateUser u);
+
+        [OperationContract]
+        string Delete(DeleteUser d);
+
+        //For Item Starts here
+        [OperationContract]
+        string Insert_Item(InsertItem item);
+
+
+    }
+
+    [DataContract]
+    public class gettestdata
+    {
+        [DataMember]
+        public DataTable usertable
+        {
+            get;
+            set;
+        }
+    }
+
+    // Use a data contract as illustrated in the sample below to add composite types to service operations.
+    [DataContract]
+    public class InsertUser
+    {
+        //Here we are declaring variable of the columns of our table
+        string name = string.Empty;
+        string email = string.Empty;
+
+        //Declaring two data member one for Displaying user's name and the other for displaying the user's mail address
+        [DataMember]
+        public string Name
+        {
+            get { return name; }
+            set { name = value; }
+        }
+
+        [DataMember]
+        public string Email
+        {
+            get { return email; }
+            set { email = value; }
+        }
+    }
+
+    [DataContract]
+    public class UpdateUser
+    {
+        int uid;
+        string name;
+        string email;
+
+        [DataMember]
+        public int UID
+        {
+            get { return uid;  }
+            set { uid = value; }
+        }
+
+        [DataMember]
+        public string Name
+        {
+            get { return name; }
+            set { name = value; }
+        }
+
+        [DataMember]
+        public string Email
+        {
+            get { return email; }
+            set { email = value; }
+        }
+    }
+
+    [DataContract]
+    public class DeleteUser
+    {
+        int uid;
+
+        [DataMember]
+        public int UID
+        {
+            get { return uid; }
+            set { uid = value; }
+        }
+    }
+
+    //For Item starts here Data Contract
+    [DataContract]
+    public class InsertItem
+    {
+        //Here we are declaring variable of the columns of our table
+        string iname = string.Empty;
+        string icategory = string.Empty;
+        string price = string.Empty;
+
+        //Declaring two data member one for Displaying user's name and the other for displaying the user's mail address
+        [DataMember]
+        public string IName
+        {
+            get { return iname; }
+            set { iname = value; }
+        }
+
+        [DataMember]
+        public string ICategory
+        {
+            get { return icategory; }
+            set { icategory = value; }
+        }
+
+        [DataMember]
+        public string Price
+        {
+            get { return price; }
+            set { price = value; }
+        }
+    }
+}
---
+++ Service1.svc.cs
@@ -0,0 +1,129 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.ServiceModel.Web;
+using System.Text;
+
+namespace Project_User_Registration
+{
+    // NOTE: You can use the "Rename" command on the "Refactor" menu to change the class name "Service1" in code, svc and config file together.
+    // NOTE: In order to launch WCF Test Client for testing this service, please select Service1.svc or Service1.svc.cs at the Solution Explorer and start debugging.
+    public class Service1 : IService1
+    {
+        public string Insert(InsertUser user) //should be equal to the one declared in IService1.cs
+        {
+            string msg;
+            SqlConnection con = new SqlConnection("data source =.; initial catalog = Grains; integrated security = True;multipleactiveresultsets = True; application name = EntityFramework");
+
+            con.Open();
+
+            SqlCommand cmd = new SqlCommand("Insert into UserTable (Name,Email) values (@Name,@Email)",con);
+
+            cmd.Parameters.AddWithValue("@Name", user.Name);
+            cmd.Parameters.AddWithValue("@Email", user.Email);
+
+            int g = cmd.ExecuteNonQuery();
+            if(g==1)
+            {
+                msg = "Sucessfully Inserted";
+            }
+            else
+            {
+                msg = "Failed to Insert";
+            }
+            return msg; 
+        }
+        
+        public gettestdata GetInfo()
+        {
+            gettestdata g = new gettestdata();
+
+            SqlConnection con = new SqlConnection("data source =.; initial catalog = Grains; integrated security = True;multipleactiveresultsets = True; application name = EntityFramework");
+
+            con.Open();
+
+            SqlCommand cmd = new SqlCommand("Select * from UserTable", con);
+            SqlDataAdapter da = new SqlDataAdapter(cmd);
+            DataTable dt = new DataTable();
+            da.Fill(dt);
+            g.usertable = dt;
+            return g;
+        }
+
+        public string Delete(DeleteUser d)
+        {
+            string msg = "";
+
+            SqlConnection con = new SqlConnection("data source =.; initial catalog = Grains; integrated security = True;multipleactiveresultsets = True; application name = EntityFramework");
+            con.Open();
+            SqlCommand cmd = new SqlCommand("delete UserTable where UserID=@UserID",con);
+            cmd.Parameters.AddWithValue("@UserID", d.UID);
+            int res = cmd.ExecuteNonQuery();
+
+            if(res==1)
+            {
+                msg = "Successfully Deleted";
+            }
+            else
+            {
+                msg = "Failed to Delete";
+            }
+            return msg;
+        }
+
+        public string Update(UpdateUser u)
+        {
+            string Message = "";
+
+            SqlConnection con = new SqlConnection("data source =.; initial catalog = Grains; integrated security = True;multipleactiveresultsets = True; application name = EntityFramework");
+
+            con.Open();
+
+            SqlCommand cmd = new SqlCommand("Update UserTable set Name=@Name, Email=@Email where UserID=@UserID", con);
+            cmd.Parameters.AddWithValue("@UserID", u.UID);
+            cmd.Parameters.AddWithValue("@Name", u.Name);
+            cmd.Parameters.AddWithValue("@Email", u.Email);
+
+            int res = cmd.ExecuteNonQuery();
+            if(res==1)
+            {
+                Message = "Successfully Update";
+            }
+            else
+            {
+                Message = "Failed to Update";
+            }
+            return Message;
+        }
+
+        //Item Insert Operation
+        public string Insert_Item(InsertItem item) //should be equal to the one declared in IService1.cs
+        {
+            string msg;
+            SqlConnection con = new SqlConnection("data source =.; initial catalog = Grains; integrated security = True;multipleactiveresultsets = True; application name = EntityFramework");
+
+            con.Open();
+
+            SqlCommand cmd = new SqlCommand("Insert into Item_Table (IName,ICategory,Price) values (@IName,@ICategory,@Price)", con);
+
+            cmd.Parameters.AddWithValue("@IName", item.IName);
+            cmd.Parameters.AddWithValue("@ICategory", item.ICategory);
+            cmd.Parameters.AddWithValue("@Price", item.Price);
+
+            int g = cmd.ExecuteNonQuery();
+            if (g == 1)
+            {
+                msg = "Sucessfully Inserted";
+            }
+            else
+            {
+                msg = "Failed to Insert";
+            }
+            return msg;
+        }
+    }
+}
